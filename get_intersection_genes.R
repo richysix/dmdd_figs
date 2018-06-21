e3a7a7acab75254e162bb284c7cad508ea38ed3b
Richard White <richard.white@sanger.ac.uk>
2018-06-21 14:13:04 +0100

    Change get_intersection_genes.R to return genes in at least a specified number of mutants rather than all.

--- get_intersection_genes.R
+++ get_intersection_genes.R
@@ -1,5 +1,8 @@
+# get genes present in at least a supplied number of lines
 Args <- commandArgs()
 upset_wide <- read.delim(Args[6])
-intersect_all <- upset_wide[ rowSums(upset_wide[,seq(2,length(upset_wide),1)]) == length(upset_wide) - 1, ]
-write.table(as.data.frame(intersect_all$gene), file = Args[7], quote = FALSE,
+out_file <- Args[7]
+threshold <- ifelse(is.na(Args[8]), length(upset_wide) - 1, Args[8])
+intersect_all <- upset_wide[ rowSums(upset_wide[,seq(2,length(upset_wide),1)]) >= threshold, ]
+write.table(as.data.frame(intersect_all$gene), file = out_file, quote = FALSE,
 row.names = FALSE, col.names = FALSE)
