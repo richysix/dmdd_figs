9285af106ded6556fff00c1f3244310c0d12de49
Richard White <richard.white@sanger.ac.uk>
2019-02-06 10:17:20 +0000

    BugFix. Command line argument needs to be converted from string to integer

--- get_intersection_genes.R
+++ get_intersection_genes.R
@@ -2,7 +2,7 @@
 Args <- commandArgs()
 upset_wide <- read.delim(Args[6])
 out_file <- Args[7]
-threshold <- ifelse(is.na(Args[8]), length(upset_wide) - 1, Args[8])
+threshold <- ifelse(is.na(Args[8]), length(upset_wide) - 1, as.integer(Args[8]) )
 intersect_all <- upset_wide[ rowSums(upset_wide[,seq(2,length(upset_wide),1)]) >= threshold, ]
 write.table(as.data.frame(intersect_all$gene), file = out_file, quote = FALSE,
 row.names = FALSE, col.names = FALSE)
